6d9763d035843180a09de9eed1a49429e6a83767
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-07-08 21:43:32 +0400

    HTML - Chapter 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,16 @@ document.getElementsByTagName("h1")[0].onmouseover = function () {
   this.innerText = "Active Heading";
   this.style.color = "red";
 };
+function showkey(e) {
+  if (e.keyCode === 89 || e.keyCode === 121) {
+    alert("Y pressed. Thank you");
+  }
+}
+document.onkeydown = showkey;
+
+function toggle() {
+  const chart = document.getElementById("chart");
+  let hid = chart.style.visibility !== "visible";
+  chart.style.visibility = hid ? "visible" : "hidden";
+  chart.style.height = hid ? "auto" : "0px";
+}
